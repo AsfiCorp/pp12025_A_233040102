2e8924159b464949945b6ef80b8b194c8a9d6384
AsfiCorp <137288269+AsfiCorp@users.noreply.github.com>
2025-02-17 11:06:23 +0700

    tugas 4 bagian b

--- nodemain.java
+++ nodemain.java
@@ -3,12 +3,17 @@ package pertemuan2;
 public class nodemain {
 	public static void main(String[] args) {
 		
-		// membuat 2 buah Node n1 & n2
-		node n1 = new node(5);
-		node n2 = new node (7);
-		
+		// tes 4 a
+		node n1 = new node (2);
+		node n2 = new node (3);
+		node n3 = new node (5);
+		node n4 = new node (7);
+		node n5 = new node (9);
 		// Membuat relasi Node n1 & n2
 		n1.setNext(n2);
+		n2.setNext(n3);
+		n3.setNext(n4);
+		n4.setNext(n5);
 		
 		// Menampilkan Node n1 & n2 dengan Pointer P
 		node p = n1;
@@ -18,6 +23,6 @@ public class nodemain {
 			p = p.getNext();
 		}
 	}
-}
+} 
 
 //Athaillah Sulthan F.I
